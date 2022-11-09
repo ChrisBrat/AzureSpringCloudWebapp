8296e74fb977fa623f8d64251dd5d30eda702cf3
Chris Braithwaite <chris.braithwaite@entelect.co.za>
2022-11-09 23:51:46 +0200

    Connect services

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -3,17 +3,44 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 
-
+@Slf4j
 @RestController
 public class HelloController {
 
 	@Value("${helloworld.message:HelloWorldNotSet}")
 	private String helloWorld;
 
+	@Autowired
+    private DiscoveryClient discoveryClient;
+
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot! : "+helloWorld;
+
+		log.info("--- Finding service discovery URI");
+		discoveryClient.getServices().stream().forEach(System.err::println);
+		log.info("--- Found services :  "+(!discoveryClient.getInstances("appapi").isEmpty()));
+
+		String serviceURI = discoveryClient.getInstances("appapi").stream().findFirst().get().getUri().toString();
+		
+		log.info(">>>>>> Making request to 'appapi' : {} ",serviceURI);		
+
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.getForEntity(serviceURI , String.class);
+
+		log.info("<<<<  Response '{}' ",response);		
+
+		return response.getBody();
+	}
+
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "Hello message from config service > "+helloWorld;
 	}
 
 }
