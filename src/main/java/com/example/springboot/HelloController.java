608205f4c44ce6e108060480f973fbc9979f19f2
Chris Braithwaite <chris.braithwaite@entelect.co.za>
2022-11-09 21:35:30 +0200

    Initial checkin

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -2,13 +2,18 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
 
 @RestController
 public class HelloController {
 
+	@Value("${helloworld.message:HelloWorldNotSet}")
+	private String helloWorld;
+
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "Greetings from Spring Boot! : "+helloWorld;
 	}
 
 }
